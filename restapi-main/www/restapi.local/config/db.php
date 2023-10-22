e44bb428cbaf7a57e3311599d52f90dfa3218c15
kipzx <145884593+kipzx@users.noreply.github.com>
2023-10-22 13:01:48 +0500

    Add files via upload

---
+++ restapi-main/www/restapi.local/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=restapi_mysql_1;dbname=restapi',
+    'username' => 'root',
+    'password' => 'Mirolsli1',
+    'charset' => 'utf8mb4',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
